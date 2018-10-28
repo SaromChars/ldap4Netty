522137c421ffe408d2959ef49749d28111413179
SaromChars <saromc@qq.com>
2018-10-29 06:28:22 +0800

    initializes the project and setting , 初始化项目以及相关配置(.gitignore)â

---
+++ ldap-server/src/main/java/priv/sarom/ldap4Netty/App.java
@@ -0,0 +1,13 @@
+package priv.sarom.ldap4Netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
