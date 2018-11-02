2424c4064595071a05ea41d8dba63e309fc646af
saromc <saromc@qq.com>
2018-11-02 08:50:36 +0800

    delete the unnecesary code

--- ldap-server/src/main/java/priv/sarom/ldap4Netty/App.java
+++ ldap-server/src/main/java/priv/sarom/ldap4Netty/App.java
@@ -2,21 +2,9 @@ package priv.sarom.ldap4Netty;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.util.ExecutorServices;
-import org.hibernate.validator.cdi.HibernateValidator;
-import org.hibernate.validator.cdi.internal.interceptor.MethodValidated;
-import priv.sarom.ldap4Netty.demo.MultiServer;
-import priv.sarom.ldap4Netty.demo.handler.EchoServerHandler;
-import priv.sarom.ldap4Netty.demo.handler.TimeServerHandler;
 import priv.sarom.ldap4Netty.ldap.LDAPServer;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import javax.validation.executable.ExecutableValidator;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,9 +20,6 @@ public class App {
                 .run();*/
 
         Configurator.setRootLevel(Level.DEBUG);
-
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        factory.getValidator().forExecutables();
 
         LDAPServer ldapServer = new LDAPServer(null);
 
